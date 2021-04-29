5e6dc903b069247b1cbc00c297e26a63488cef61
antspartanelite <antspartanelite@gmail.com>
2021-04-29 15:36:39 +0100

    Added new and open document options in menu

--- project.py
+++ project.py
@@ -2,12 +2,37 @@ import tkinter as tk
 import os
 import time
 from tkinter import *
+from tkinter import filedialog
 from PIL import Image,ImageTk
 from pdf2image import convert_from_path
 
 texFilePath = "my_file.tex"
 
 has_prev_key_release = None
+
+def newDocument():
+    textBox.delete("1.0", END)
+    displayBox.configure(state="normal")
+    displayBox.delete("1.0", END)
+    displayBox.configure(state="disabled")
+
+def openDocument():  
+    latexFile = filedialog.askopenfilename(filetypes=(("TeX Files", "*.tex"),))
+    
+    if len(latexFile) == 0:
+        return
+    
+    textBox.delete("1.0", END)
+    displayBox.configure(state="normal")
+    displayBox.delete("1.0", END)
+    displayBox.configure(state="disabled")
+
+    latexFile = open(latexFile, "r")
+    content = latexFile.read()
+
+    textBox.insert(END, content)
+    latexFile.close()
+    updateTexFile(textBox, "my_file.tex", displayBox)
     
 
 def on_key_release(textBox, texFilePath, displayBox):
@@ -62,6 +87,8 @@ def updateDisplayBox(displayBox, texFilePath):
     displayBox.yview_moveto(scrollPos[0])
     displayBox.after(3, displayBox.yview_moveto, scrollPos[0])
 
+
+
 window = Tk()
 window.title("Python LaTeX editor")
 window.geometry("1620x700")
@@ -69,17 +96,25 @@ window.geometry("1620x700")
 textFrame = Frame(window)
 textFrame.pack(pady=10)
 
+# Adding Scrollbar to the text frame
+textScrollY = Scrollbar(textFrame,orient=VERTICAL)
+
 # Adding Scrollbar to the PDF frame
 pdfScrollY = Scrollbar(textFrame,orient=VERTICAL)
 
 #Sets height of texboxes relative to font
-textBox = Text(textFrame, width=88, height=33, font=("Helvetica", 12), selectbackground="yellow", selectforeground="black")
+textBox = Text(textFrame, width=88, height=33, font=("Helvetica", 12), yscrollcommand = textScrollY.set, selectbackground="blue", selectforeground="white")
 textBox.pack(side=tk.LEFT,padx=10)
 displayBox = Text(textFrame, width=66, height=25, font=("Helvetica", 16), yscrollcommand = pdfScrollY.set, selectbackground="yellow", selectforeground="black", bg="grey")
 
-# Setting the scrollbar to the right side
+
+# Setting the scrollbar to the right side of each frame
+textScrollY.pack(side=LEFT,fill=Y)
+textScrollY.config(command=textBox.yview)
 pdfScrollY.pack(side=RIGHT,fill=Y)
 pdfScrollY.config(command=displayBox.yview)
+
+
 
 displayBox.pack(side=tk.RIGHT,padx=10)
 textBox.bind("<KeyRelease>", lambda event, arg=(0): on_key_release_repeat(textBox, texFilePath, displayBox)) 
@@ -97,5 +132,25 @@ f.close()
 displayBox.configure(state="disabled")
 
 updateDisplayBox(displayBox, texFilePath)
+
+myMenu = Menu(window)
+window.config(menu = myMenu)
+
+fileMenu = Menu(myMenu)
+myMenu.add_cascade(label="File",menu=fileMenu)
+fileMenu.add_command(label="New", command=newDocument)
+fileMenu.add_command(label="Open", command=openDocument)
+fileMenu.add_command(label="Save")
+fileMenu.add_command(label="Save As")
+fileMenu.add_command(label="Export")
+
+editMenu = Menu(myMenu)
+myMenu.add_cascade(label="Edit",menu=editMenu)
+editMenu.add_command(label="Undo")
+editMenu.add_command(label="Redo")
+editMenu.add_command(label="Cut")
+editMenu.add_command(label="Copy")
+editMenu.add_command(label="Paste")
+
 
 window.mainloop()
